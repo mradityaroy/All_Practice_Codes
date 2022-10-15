0fa8eaf93d714760ce9ab6738ffab80e76128453
Aditya Kumar <mradityaroy001@gmail.com>
2022-10-15 11:24:18 +0530

    .

---
+++ newproj/venv/flask_variable_dynamic_web_page.py
@@ -0,0 +1,15 @@
+from unicodedata import name
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/hello/<int:postID>')
+def show_blog(postID):
+    return 'Blog Number %d' % postID
+
+@app.route('/rev/<float:revNo>')
+def revision(revNo):
+    return 'Revision Number %f' % revNo
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ newproj/venv/hello_<name>.py
@@ -0,0 +1,11 @@
+from unicodedata import name
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/hello/<name>')
+def hello_world(name):
+    return 'Hello %s!' % name
+
+if __name__ == '__main__':
+    app.run(debug=True)
